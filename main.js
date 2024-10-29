771606c182a012fd5f3b08ada8e1512fa8423b8b
loglot <88983354+loglot@users.noreply.github.com>
2024-10-29 14:34:40 +0000

    commiting left behind changes

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+document.getElementById("debug").innerHTML = "W not pressed";
+
+
 import { Imports } from "./lib/import.js";
+import kd, { tick } from "./lib/utills/KeyManager.js";
 
 game = new Imports(this)
 
+kd.run()
+
+requestAnimationFrame(stick)
+
+function stick(){
+    document.getElementById("debug").innerHTML = "W not pressed";
+    if(kd.W.isDown()){
+        document.getElementById("debug").innerHTML = "W pressed";
+    }
+}
