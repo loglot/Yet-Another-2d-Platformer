722780107325ad81dd12dd0c61cd8ccc99940363
loglot <88983354+loglot@users.noreply.github.com>
2024-10-31 13:36:56 +0000

    changed some more with camera shake, and added debug system

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-document.getElementById("debug").innerHTML = "W not pressed";
 
 
 import { Imports } from "./lib/import.js";
@@ -11,19 +10,6 @@ requestAnimationFrame(stick)
 
 function stick(){
     // document.getElementById("debug").innerHTML = `A not pressed ${game.player.x}`;
-    if(kd.A.isDown()){
-        game.player.velX--
-        // document.getElementById("debug").innerHTML = `A pressed ${game.player.x}`;
-    }
-    if(kd.S.isDown()){
-        game.player.velY++
-    }
-    if(kd.D.isDown()){
-        game.player.velX++
-    }
-    if(kd.W.isDown()){
-        game.player.velY--
-    }
     if(kd.RIGHT.isDown()){
         game.camera.velR = -50
     }
@@ -31,17 +17,13 @@ function stick(){
         game.camera.velR = 50
     }
     if(kd.Q.isDown()){
-        game.camera.movementFactor = 10
-    }
-    if(kd.E.isDown()){
-        game.camera.movementFactor = 100
-        game.player.velX =0
-        game.player.velY =0
+        game.camera.ImpactCameraShake()
     }
     game.camera.tick()
     game.player.tick()
 
     game.display.tick()
     kd.tick()
+    game.printDebug()
     requestAnimationFrame(stick)
 }
