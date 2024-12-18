73aac14c881508e63eea8a4dd6cb2d15c9fd5a64
LogLot <prattlog3@gmail.com>
2024-12-18 16:55:53 -0500

    painnnn

--- Electron.js
+++ Electron.js
@@ -1,23 +1,22 @@
 const {protocol, app, BrowserWindow } = require('electron')
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 192000,
-      height: 108000,
-      icon: __dirname + '/icon.ico',
-      // transparent: true, frame: false
-    })
-  
-    win.loadFile('index.html')
-  }
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+const win = new BrowserWindow({
+  width: 192000,
+  height: 108000,
+  icon: __dirname + '/icon.ico',
+  transparent: true, frame: false
+})
+
+  win.loadFile('index.html')
+}
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+app.whenReady().then(() => {
+  createWindow()
+  win.setFullScreen(true)
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-  app.whenReady().then(() => {
-    createWindow()
-    win.setFullScreen(true)
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-  
+})
